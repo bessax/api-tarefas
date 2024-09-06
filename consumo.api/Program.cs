982f5c9d8b4c367bf2c6ba21d17a129a2db6e690
bessax <andrebessax@gmail.com>
2024-09-05 21:38:28 -0300

    #update: Adição de uma aplicação console para consumir a API.

---
+++ consumo.api/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using consumo.api;
+using System.Net.Http.Json;
+
+var client = new HttpClient();
+
+async Task ConsultaTarefas()
+{
+    var url = "http://localhost:5273/api/tarefa";
+    var response = await client.GetAsync(url);
+    var resultado = await response.Content.ReadFromJsonAsync<List<TarefaDTO>>();
+
+    foreach (var item in resultado!)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+async Task CadastraTarefa()
+{
+    var url = "http://localhost:5273/api/tarefa";
+
+    var tarefaDTO = new TarefaDTO() { 
+          Id= Guid.NewGuid(),
+          Descricao = "Tarefa de Teste",
+          Nome="Teste"
+         };
+
+    var content = await client.PostAsJsonAsync<TarefaDTO>(url,tarefaDTO);
+
+    var resultado = await content.Content.ReadAsStringAsync();
+
+    Console.WriteLine(resultado);
+  }
+
+async Task ConsultaTarefaPorId()
+{
+    var url = "http://localhost:5273/api/tarefa/a4b0b31b-ddef-4788-a636-8727f55c583a";
+    var response = await client.GetAsync(url);
+    var resultado = await response.Content.ReadFromJsonAsync<TarefaDTO>();
+       Console.WriteLine(resultado);
+
+}
+
+await CadastraTarefa();
+
+
---
+++ consumo.api/TarefaDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consumo.api;
+
+public class TarefaDTO
+{
+    public Guid Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+
+    public override string ToString()
+    {
+        return $"ID= {Id} - Nome= {Nome} - Descrição= {Descricao}";
+    }
+}
+
